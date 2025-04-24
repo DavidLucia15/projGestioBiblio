791d2113dd91164822c865f02fc72bd9bd9fbd76
DavidLucia15 <david.lucia@insbaixcamp.cat>
2025-04-24 17:25:42 +0200

    CommitAmbCategoria

--- Llibre.java
+++ Llibre.java
@@ -2,16 +2,19 @@ public class Llibre{
     private String titol;
     private String autor;
     private boolean prestat;
+    private String categoria;
 
-    public Llibre(String titol, String autor) {
+    public Llibre(String titol, String autor, String categoria) {
     this.titol = titol;
     this.autor = autor;
     this.prestat = false;
+    this.categoria = categoria;
     }
 
     public String getTitol() { return titol; }
     public String getAutor() { return autor; }
     public boolean esPrestat() { return prestat; }
+    public String getCategoria() {return this.categoria;}
     public void prestar() { prestat = true; }
     public void retornar() { prestat = false; }
 
