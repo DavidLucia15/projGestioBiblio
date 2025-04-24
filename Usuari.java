997b6dd03f1d1042ea240d29b6d46eaf66aeccd7
Fernando Ortiz <Fernando.Work.Account@proton.me>
2025-04-24 17:53:05 +0200

    Aqui esta todo lo de añadir un historial para cada usuario

--- Usuari.java
+++ Usuari.java
@@ -2,15 +2,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Usuari {
-private String nom;
-private List<Llibre> llibresPrestats;
-public Usuari(String nom) {
-this.nom = nom;
-this.llibresPrestats = new ArrayList<>();
-}
+    private String nom;
+    private List<Llibre> Historial;
+    private List<Llibre> llibresPrestats;
 
-public String getNom() { return nom; }
-public List<Llibre> getLlibresPrestats() { return llibresPrestats; }
-public void afegirLlibre(Llibre llibre) { llibresPrestats.add(llibre); }
-public void retornarLlibre(Llibre llibre) { llibresPrestats.remove(llibre); } 
+    public Usuari(String nom) {
+        this.nom = nom;
+        this.Historial = new ArrayList<>(); // Inicialitzem el historial
+        this.llibresPrestats = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void afegirHistorial(Llibre llibre) {
+        Historial.add(llibre); 
+    }
+
+    public List<Llibre> getLlibresPrestats() {
+        return llibresPrestats;
+    }
+
+    public void afegirLlibre(Llibre llibre) {
+        afegirHistorial(llibre);
+        llibresPrestats.add(llibre);
+    }
+
+    public void retornarLlibre(Llibre llibre) {
+        llibresPrestats.remove(llibre);
+        afegirHistorial(llibre); // Puedes aprovechar para moverlo al historial
+    }
+
+    public List<Llibre> getHistorial() {
+        return Historial;
+    }
 }
